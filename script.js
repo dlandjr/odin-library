a848f8c572af30dc6ef2e028fc28c55ac038c2fa
dlandjr <donald.landjr@gmail.com>
2025-04-20 15:12:31 -0700

    Finalize code for submission

--- script.js
+++ script.js
@@ -68,26 +68,40 @@ class UI {
         this.library.books.forEach(book => this.createCard(book));  
     }
 
-    createCard (book) {
+    createCard(book) {
         const newCard = cardTemplate.content.cloneNode(true);
         const card = newCard.querySelector('.card');
         card.id = book.id;
-
+      
         newCard.querySelector('.book-title').textContent = book.title;
         newCard.querySelector('.book-title').href = book.link || '#';
         newCard.querySelector('.year').textContent = book.year;
         newCard.querySelector('.author').textContent = book.author;
-
+      
         if (book.cover) {
-            newCard.querySelector('.book-title').classList.add('hover');
-            newCard.querySelector('.cover').src = book.cover;
+          newCard.querySelector('.book-title').classList.add('hover');
+          newCard.querySelector('.cover').src = book.cover;
         }
-
+      
         const delButton = newCard.querySelector('.del');
         delButton.addEventListener('mousedown', () => this.deleteCard(book.id, card));
-
+      
+        const status = newCard.querySelector('.read-status');
+        if (book.read) {
+            card.classList.add('read');
+            status.textContent = 'Read';
+        } else {
+            status.textContent = 'Unread';
+        }
+        
+        status.addEventListener('click', () => {
+            book.read = !book.read;
+            card.classList.toggle('read', book.read);
+            status.textContent = book.read ? 'Read' : 'Unread';
+        });
+      
         bookList.insertBefore(newCard, bookList.firstChild);
-    }
+    }      
 
     deleteCard(id, card) {
         this.library.removeBook(id);
@@ -104,9 +118,9 @@ const themes = {
         '--bg-col': '#1c232b',           
         '--input-bg': '#5c6d7c',
         '--true-col': '#6eb1c0',         
-        '--false-col': '#9a6ca',        
+        '--false-col': '#9a6ca3',        
         '--accent-col': '#5f72a6',       
-        '--accent2-col': '#6f8ab7',    
+        '--accent2-col': '#66dcf6',    
         '--text': '#f1eee9',  
     modeText: 'light_mode'
     },
@@ -178,52 +192,64 @@ const ui = new UI(library);
         author: 'John Steinbeck', 
         year: 1952, 
         link:'https://www.goodreads.com/book/show/4406.East_of_Eden?ac=1&from_search=true&qid=bQQcyxpIPi&rank=1', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1639969375i/4406.jpg'
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1639969375i/4406.jpg',
+        read: true
     },
 
     {title: 'Lonesome Dove', 
         author: 'Larry McMurtry', 
         year: 1985, 
         link:'https://www.goodreads.com/book/show/256008.Lonesome_Dove?from_search=true&from_srp=true&qid=siFTxDA130&rank=1', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1559668037i/256008.jpg'
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1559668037i/256008.jpg',
+        read: true
     },
 
     {title: 'The Brothers K', 
         author: 'David James Duncan', 
         year: 1992, 
         link:'https://www.goodreads.com/book/show/19534.The_Brothers_K?ref=nav_sb_ss_2_14',
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1403176622i/19534.jpg'
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1403176622i/19534.jpg',
+        read: true
     },
 
     {title: 'Barbarian Days: A Surfing Life', 
         author: 'William Finnegan', 
         year: 2015, 
         link:'https://www.goodreads.com/book/show/18693910-barbarian-days?ref=nav_sb_ss_1_14', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1418768620i/18693910.jpg'},
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1418768620i/18693910.jpg',
+        read: true
+    },
 
     {title: 'The Pragmatic Programmer: From Journeyman to Master', 
         author: 'Dave Thomas', 
         year: 1999, 
         link:'https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer?ref=nav_sb_ss_1_8',
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'},
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        read: false
+    },
 
     {title: 'When Breath Becomes Air', 
         author: 'Paul Kalanithi', 
         year: 2016, 
         link:'https://www.goodreads.com/book/show/25899336-when-breath-becomes-air?ref=nav_sb_ss_1_11', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1492677644i/25899336.jpg'},
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1492677644i/25899336.jpg',
+        read: true
+    },
 
     {title: 'Cutting for Stone', 
         author: 'Abraham Verghese', 
         year: 2009, 
         link:'https://www.goodreads.com/book/show/3591262-cutting-for-stone?ref=nav_sb_ss_1_17', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1553715166i/3591262.jpg'},
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1553715166i/3591262.jpg',
+        read: true
+    },
 
     {title: 'Cadillac Desert: The American West and Its Disappearing Water', 
         author: 'Marc Reisner', 
         year: 1986, 
         link:'https://www.goodreads.com/book/show/56140.Cadillac_Desert?ref=nav_sb_ss_1_8', 
-        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1388189076i/56140.jpg'}
+        cover:'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1388189076i/56140.jpg',
+        read: true }
 ].forEach(bookData => {
     const book = new Book(
         bookData.title, 
